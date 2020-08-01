6f57ba94f4495dd52749270b9759a5bd8f1d6614
无忧 <1097810498@qq.com>
2020-08-01 14:24:05 +0800

    修复打包问题

--- src/main/java/com/wuyou/robot/BotRunApplication.java
+++ src/main/java/com/wuyou/robot/BotRunApplication.java
@@ -1,7 +1,5 @@
 package com.wuyou.robot;
 
-import com.forte.component.forcoolqhttpapi.CQHttpContext;
-import com.forte.qqrobot.SimpleRobotApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,11 +13,9 @@ import org.springframework.context.annotation.ComponentScan;
  */
 
 @SpringBootApplication
-@SimpleRobotApplication(resources = "/resources/application.properties")
 @MapperScan("com.wuyou.mapper")
 @ComponentScan("com.wuyou")
 public class BotRunApplication {
-	public static CQHttpContext run;
 
 	public static void main(String[] args){
 		// new 一个cqhttp组件的启动器，并将你的启动器类放进去。
